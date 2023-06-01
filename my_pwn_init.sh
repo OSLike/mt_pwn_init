3371f203dfad16748b30a8663956d7d6f13e5cee
OSLike <112612464+OSLike@users.noreply.github.com>
2023-06-01 15:13:32 +0800

    Add files via upload

---
+++ my_pwn_init.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Hello, Pwn! -- from Nsus"
+echo ""
+echo ""
+
+cd ~
+
+echo "I think that u have been changed apt source.."
+
+if [ ! -d ~/.pip ]; then
+	mkdir ~/.pip
+fi
+
+echo -e "[global]\nindex-url = https://pypi.douban.com/simple/\n[install]\ntrusted-host = pypi.douban.com" >  ~/.pip/pip.conf
+
+echo "time to use ur apt to install something mecessary"
+
+dpkg --add-architecture i386
+sudo apt update
+sudo apt -y install libc6-i386
+sudo apt -y install git gdb python3-pip
+
+echo "time to use git to clone pwndbg and setup it"
+
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+
+echo "this time i will install some useful tools for u"
+
+sudo apt source libc6-dev
+echo "this is pwntools"
+pip install pwntools
+echo "this is ruby, it can help u install one_gadget and seccomp-tools"
+sudo apt install ruby
+sudo gem install one_gadget
+sudo gem install seccomp-tools
+echo "this is patchelf, change a ELF's ld and libc"
+sudo apt install patchelf
+echo "this is glibc-all-in-one"
+sudo git clone https://github.com/matrix1001/glibc-all-in-one.git
+echo "this is LibcSearcher"
+pip install LibcSearcher
+
+echo "Pwngdb! heap!heap!"
+cd ~
+git clone https://github.com/scwuaptx/Pwngdb.git
+cp ~/Pwndbg/.gdbinit ~
+
+echo "no peda!:("
+sed -i 's/peda/pwndbg/' ~/.gdbinit
+sed -i 's/peda/gdbinit/' ~/.gdbinit
